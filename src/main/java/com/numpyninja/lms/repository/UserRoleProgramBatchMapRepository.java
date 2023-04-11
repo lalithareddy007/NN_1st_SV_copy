56d8bb0bd0a9c6db93552479ba5b4d69318a1550
NumPyNinja <68971671+NumPyNinja@users.noreply.github.com>
2023-04-11 18:39:46 -0400

    Revert "Revert "Feature/get users by program""

--- src/main/java/com/numpyninja/lms/repository/UserRoleProgramBatchMapRepository.java
+++ src/main/java/com/numpyninja/lms/repository/UserRoleProgramBatchMapRepository.java
@@ -15,6 +15,7 @@ public interface UserRoleProgramBatchMapRepository extends JpaRepository<UserRol
 
     Optional<UserRoleProgramBatchMap> findByUser_UserIdAndRoleRoleIdAndUserRoleProgramBatchStatusEqualsIgnoreCase
             (String userId, String roleId, String status);
+    List<UserRoleProgramBatchMap> findByProgram_ProgramId(Long programId);
 
     List<UserRoleProgramBatchMap> findByUser_UserId(String userId);
 
