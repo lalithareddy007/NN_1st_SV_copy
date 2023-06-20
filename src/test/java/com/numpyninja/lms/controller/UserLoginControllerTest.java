ac745576a03ec5140934e3bffc45e4d4200844d9
swati-naik <swapat3@gmail.com>
2023-06-20 17:12:33 -0500

    userlogin changes

--- src/test/java/com/numpyninja/lms/controller/UserLoginControllerTest.java
+++ src/test/java/com/numpyninja/lms/controller/UserLoginControllerTest.java
@@ -1,8 +1,10 @@
 package com.numpyninja.lms.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.numpyninja.lms.config.ApiResponse;
 import com.numpyninja.lms.dto.JwtResponseDto;
 import com.numpyninja.lms.dto.LoginDto;
+import com.numpyninja.lms.security.jwt.JwtUtils;
 import com.numpyninja.lms.services.ProgBatchServices;
 import com.numpyninja.lms.services.UserLoginService;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,7 +21,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,6 +32,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
@@ -45,6 +52,8 @@ public class UserLoginControllerTest extends AbstractTestController {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    JwtUtils jwutils;
     @Test
     public void given_NonExistingUser_WhenLogin_ThenThrowException( ) throws Exception {
         LoginDto loginDto = new LoginDto();
@@ -86,23 +95,32 @@ public class UserLoginControllerTest extends AbstractTestController {
 
 //localhost:1234/lms/login/AccountActivation
 //    @Test
-//    public void given_TokenFromValidUser_WhenClickonResetLink_ThenReturnValidity() throws Exception
-//    {
-//        String token ="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuaXRAZ21haWwuY29tIiwiaWF0IjoxNjg2MTQzMjYwLCJleHAiOjE2ODYzMTYwNjB9.QvVEiYYLxxRjAqAyrZJdSROWAQ3gP0o5uxez_Ar1Z-9MFkRXuSXt3ANok_LaZmzjKYa9d2q5DDvn3v1npgR3Kw";
-//        String validity = "success";
+//    public void given_TokenForValidUser_WhenClickonResetLink_ThenReturnApiResponse() throws Exception {
+//        String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuaXRAZ21haWwuY29tIiwiaWF0IjoxNjg2MTQzMjYwLCJleHAiOjE2ODYzMTYwNjB9.QvVEiYYLxxRjAqAyrZJdSROWAQ3gP0o5uxez_Ar1Z-9MFkRXuSXt3ANok_LaZmzjKYa9d2q5DDvn3v1npgR3Kw";
+//        String validity = "valid";
+//        String emailid = "abc@gmail.com";
+//        ApiResponse apiResponse =  new ApiResponse("Valid Token", true);
 //
 //        //given
-//        given( userLoginService.validateAccountActToken( token)).willReturn( "validity");
+//        given(jwutils.validateAccountActivationToken(token)).willReturn(emailid);
+//      //  given(validity.equalsIgnoreCase("valid")).willReturn(Boolean.valueOf(emailid));
+//        given(userLoginService.validateTokenAtAccountActivation(token)).willReturn("apiResponse");
+//
+//
 //
 //        //when
-//       ResultActions response = mockMvc.perform(get("/login/AccountActivation")
-//                       .header("Authorization", "Bearer " + token))
-//               .andExpect(status().isOk());
+//        ResultActions response = mockMvc.perform(get("/login/AccountActivation")
+//                .header("Authorization", "Bearer " + token));
 //               // .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(token)));
+//        // .andExpect(status().isOk());
+//        // .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(token)));
 //
-//       //then
+//        //then
 //
 //        response.andDo(print()).andExpect(status().isOk());
+//      //  assertEquals(200,);
+//       // response.andExpect(jsonPath("token", is(jwtResponseDto.getToken())))
+//    }
 //        //response.andExpect(jsonPath("token", is(jwtResponseDto.getToken())))
 //          //      .andExpect(jsonPath("email
 //          ", is(jwtResponseDto.getEmail())))
@@ -127,4 +145,54 @@ public class UserLoginControllerTest extends AbstractTestController {
 
 //    }
 //
+
+    @Test
+    public void testValidateAccountActToken_InvalidToken() throws Exception {
+        String token = "eyJhbGciOiJIUzUxMi.eyJzdWIiOiJuaXRAZ21haWwuY29tIiwiaWF0IjoxNjg2MTQzMjYwLCJleHAiOjE2ODYzMTYwNjB9.QvVEiYYLxxRjAqAyrZJdSROWAQ3gP0o5uxez_Ar1Z-9MFkRXuSXt3ANok_LaZmzjKYa9d2q5DDvn3v1npgR3Kw";
+        when(userLoginService.validateTokenAtAccountActivation(token))
+                .thenReturn("Invalid");
+
+        ResultActions response= mockMvc.perform(get("/login/AccountActivation")
+                .header("Authorization", "invalid_token")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Invalid/Expired Token"))
+                .andExpect(jsonPath("$.success").value(false));
+    }
+
+    @Test
+    public void testValidateAccountActToken_AlreadyActivated() throws Exception {
+       String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuaXRAZ21haWwuY29tIiwiaWF0IjoxNjg2MTQzMjYwLCJleHAiOjE2ODYzMTYwNjB9.QvVEiYYLxxRjAqAyrZJdSROWAQ3gP0o5uxez_Ar1Z-9MFkRXuSXt3ANok_LaZmzjKYa9d2q5DDvn3v1npgR3Kw";
+        when(userLoginService.validateTokenAtAccountActivation(anyString()))
+                .thenReturn("acctActivated already");
+
+        ResultActions response  = mockMvc.perform(get("/login/AccountActivation")
+                .header("Authorization", "valid_token")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("acctActivated already"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false));
+    }
+
+    @Test
+    public void testValidateAccountActToken_ValidToken() throws Exception {
+        when(userLoginService.validateTokenAtAccountActivation(anyString()))
+                .thenReturn("alpha@gmail.com");
+
+        ResultActions response  = mockMvc.perform(get("/login/AccountActivation")
+                .header("Authorization", "valid_token")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("alpha@gmail.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true));
+
+    }
 }
+
+
+
+
+
--- src/test/java/com/numpyninja/lms/services/UserLoginServiceTest.java
+++ src/test/java/com/numpyninja/lms/services/UserLoginServiceTest.java
@@ -2,6 +2,7 @@ package com.numpyninja.lms.services;
 
 import com.numpyninja.lms.dto.JwtResponseDto;
 import com.numpyninja.lms.dto.LoginDto;
+import com.numpyninja.lms.entity.UserLogin;
 import com.numpyninja.lms.repository.UserLoginRepository;
 import com.numpyninja.lms.repository.UserRoleMapRepository;
 import com.numpyninja.lms.security.UserDetailsImpl;
@@ -11,6 +12,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -22,17 +26,22 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserCache;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class UserLoginServiceTest  {
+public class UserLoginServiceTest {
     @InjectMocks
     UserLoginService userLoginService;
     @Mock
@@ -65,7 +74,7 @@ public class UserLoginServiceTest  {
 
         //given
         UserDetailsImpl userDetails = mock(UserDetailsImpl.class);
-        when( userCache.getUserFromCache( loginDto.getUserLoginEmailId())).thenReturn( userDetails);
+        when(userCache.getUserFromCache(loginDto.getUserLoginEmailId())).thenReturn(userDetails);
         Authentication authentication = mock(Authentication.class);
         when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
 
@@ -83,10 +92,121 @@ public class UserLoginServiceTest  {
         JwtResponseDto jwtResponseDtoGot = userLoginService.signin(loginDto);
 
         //then
-        Assertions.assertEquals(jwtResponseDtoGot.getEmail(), loginDto.getUserLoginEmailId());
-        Assertions.assertEquals(jwtResponseDtoGot.getToken(), jwtToken);
-        Assertions.assertEquals(jwtResponseDtoGot.getUserId(), "U11");
-        Assertions.assertEquals(jwtResponseDtoGot.getRoles(), Collections.singletonList("ROLE_STAFF"));
+        assertEquals(jwtResponseDtoGot.getEmail(), loginDto.getUserLoginEmailId());
+        assertEquals(jwtResponseDtoGot.getToken(), jwtToken);
+        assertEquals(jwtResponseDtoGot.getUserId(), "U11");
+        assertEquals(jwtResponseDtoGot.getRoles(), Collections.singletonList("ROLE_STAFF"));
     }
 
+
+//    void testvalidateTokenAtAccountActivation_Valid() throws Exception{
+//
+//        String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuaXRAZ21haWwuY29tIiwiaWF0IjoxNjg2MTQzMjYwLCJleHAiOjE2ODYzMTYwNjB9.QvVEiYYLxxRjAqAyrZJdSROWAQ3gP0o5uxez_Ar1Z-9MFkRXuSXt3ANok_LaZmzjKYa9d2q5DDvn3v1npgR3Kw";
+//       String emailID = "alpha@gmail.com";
+//        Optional<UserLogin> user = null;
+//        when(jwtUtils.validateAccountActivationToken(token)).thenReturn("Valid");
+//        when(jwtUtils.getUserNameFromJwtToken(token)).thenReturn(emailID);
+//        when(userLoginRepository.findByUserLoginEmailIgnoreCase(emailID)).thenReturn(user);
+//
+//        when(user.get().getPassword().isEmpty()).thenReturn(false);
+//
+//
+//    }
+
+//
+//    @Test
+//    public void testValidateTokenAtAccountActivation_InvalidToken() {
+//       //String token = "InvalidToken";
+//        String token = "eyJhbGciOiJIUzUxMi.eyJzdWIiOiJuaXRAZ21haWwuY29tIiwiaWF0IjoxNjg2MTQzMjYwLCJleHAiOjE2ODYzMTYwNjB9.QvVEiYYLxxRjAqAyrZJdSROWAQ3gP0o5uxez_Ar1Z-9MFkRXuSXt3ANok_LaZmzjKYa9d2q5DDvn3v1npgR3Kw";
+//        when(jwtUtils.validateAccountActivationToken(token))
+//                .thenReturn("InValid");
+//        String validity = jwtUtils.validateAccountActivationToken(token);
+//
+//        Assertions.assertEquals("Invalid", validity);
+//    }
+//
+//    @Test
+//    public void testValidateTokenAtAccountActivation_AlreadyActivated() {
+//        String token = "Bearer ValidToken";
+//        String userName = "example@email.com";
+//
+//        when(jwtUtils.validateAccountActivationToken(token))
+//                .thenReturn("Valid");
+//        when(jwtUtils.getUserNameFromJwtToken(token))
+//                .thenReturn(userName);
+//
+//        Optional<UserLogin> userOptional = Optional.of(new UserLogin());
+//        when(userLoginRepository.findByUserLoginEmailIgnoreCase(userName))
+//                .thenReturn(userOptional);
+//
+//        String validity = userLoginService.validateTokenAtAccountActivation(token);
+//
+//        Assertions.assertEquals("acctActivated already", validity);
+//    }
+//
+//    @Test
+//    public void testValidateTokenAtAccountActivation_ValidToken() {
+//        String token = "Bearer ValidToken";
+//        String userName = "example@email.com";
+//
+//        when(jwtUtils.validateAccountActivationToken(token))
+//                .thenReturn("Valid");
+//        when(jwtUtils.getUserNameFromJwtToken(token))
+//                .thenReturn(userName);
+//
+//        when(userLoginRepository.findByUserLoginEmailIgnoreCase(userName))
+//                .thenReturn(Optional.empty());
+//
+//        String validity = userLoginService.validateTokenAtAccountActivation(token);
+//
+//        Assertions. assertEquals(userName, validity);
+//    }
+
+//
+
+
+
+    @Test
+    public void testValidateTokenAtAccountActivation() {
+        // Invalid token
+        String invalidToken = "InvalidToken";
+        String invalidValidity = "Invalid";
+        assertEquals(invalidValidity, userLoginService.validateTokenAtAccountActivation(invalidToken));
+
+        // Already activated account
+        String activatedToken = "Bearer ValidToken";
+        String activatedEmail = "test@example.com";
+        String alreadyActivatedValidity = "acctActivated already";
+
+        when(jwtUtils.validateAccountActivationToken(anyString()))
+                .thenReturn("Valid");
+        when(jwtUtils.getUserNameFromJwtToken(anyString()))
+                .thenReturn(activatedEmail);
+
+        Optional<UserLogin> activatedUserOptional = Optional.of(new UserLogin());
+        when(userLoginRepository.findByUserLoginEmailIgnoreCase(activatedEmail))
+                .thenReturn(activatedUserOptional);
+
+        assertEquals(alreadyActivatedValidity, userLoginService.validateTokenAtAccountActivation(activatedToken));
+
+        // Valid token
+        String validToken = "Bearer ValidToken";
+        String validEmail = "test@example.com";
+        String validValidity = "Valid";
+
+        when(jwtUtils.validateAccountActivationToken(anyString()))
+                .thenReturn("Valid");
+        when(jwtUtils.getUserNameFromJwtToken(anyString()))
+                .thenReturn(validEmail);
+
+        when(userLoginRepository.findByUserLoginEmailIgnoreCase(validEmail))
+                .thenReturn(Optional.empty());
+
+        assertEquals(validValidity, userLoginService.validateTokenAtAccountActivation(validToken));
+    }
 }
+
+
+
+
+
