d10a8fbf86a6e8b8ed3c577c5be745125bb0bfac
Shemeeba <shemeebapunathil@gmail.com>
2023-07-20 15:16:39 -0400

    User role status
    User role status can accept only " Active, In Active , Online, Offline and Away".

--- src/main/java/com/numpyninja/lms/dto/UserRoleMapSlimDTO.java
+++ src/main/java/com/numpyninja/lms/dto/UserRoleMapSlimDTO.java
@@ -1,6 +1,7 @@
 package com.numpyninja.lms.dto;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.numpyninja.lms.entity.Role;
@@ -27,5 +28,7 @@ public class UserRoleMapSlimDTO {
 	
 	@NotEmpty(message = "Role Status is Mandatory")
 	@JsonProperty("userRoleStatus")
+	@Pattern(regexp = "active|inactive|online|offline|away", flags = Pattern.Flag.CASE_INSENSITIVE,
+    message = "User-Role Status should be Active, InActive, Online, Offline,or Away")
 	private String userRoleStatus;
 }
