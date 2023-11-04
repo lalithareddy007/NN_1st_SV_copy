91c10ec7a56e252fff5542a07504949556aedff6
Akshata Kanaje <akshatakanaje@Akshatas-MacBook-Pro.local>
2023-11-03 17:18:16 -0700

    added negative scenarios for assignmnet integration testing

--- src/test/java/com/numpyninja/lms/controller/AssignmentControllerIT.java
+++ src/test/java/com/numpyninja/lms/controller/AssignmentControllerIT.java
@@ -2,19 +2,17 @@ package com.numpyninja.lms.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.numpyninja.lms.config.ApiResponse;
 import com.numpyninja.lms.dto.AssignmentDto;
 import com.numpyninja.lms.dto.JwtResponseDto;
 import com.numpyninja.lms.dto.LoginDto;
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -118,6 +116,124 @@ public class AssignmentControllerIT {
 
     @Test
     @Order(2)
+    public void testAlreadyExistAssignment() throws Exception {
+        final AssignmentDto assignmentDto = new AssignmentDto();
+        assignmentDto.setAssignmentName("DA Assignment");
+        assignmentDto.setAssignmentDescription("DA Practice");
+        assignmentDto.setComments("New assignment created");
+        assignmentDto.setPathAttachment1("FilePath1");
+        assignmentDto.setPathAttachment2("FilePath2");
+        assignmentDto.setPathAttachment3("FilePath3");
+        assignmentDto.setPathAttachment4("FilePath4");
+        assignmentDto.setPathAttachment5("FilePath5");
+        assignmentDto.setCreatedBy("U01");
+        assignmentDto.setBatchId(1);
+        assignmentDto.setGraderId("U01");
+        Calendar calendar = (Calendar) Calendar.getInstance();
+        calendar.set(Calendar.MONTH, 10);
+        calendar.set(Calendar.YEAR, 2023);
+        calendar.set(Calendar.DAY_OF_WEEK, 14);
+        Date dt = calendar.getTime();
+        assignmentDto.setDueDate(dt);
+
+        String jsonRequest = obj.writeValueAsString(assignmentDto);
+        MvcResult mvcResult = mockMvc.perform(post("/lms/assignments").contextPath("/lms")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType("application/json")
+                        .content(jsonRequest))
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        assertEquals(400, mvcResult.getResponse().getStatus());
+
+        ApiResponse apiResponse = obj.readValue(jsonResponse, ApiResponse.class);
+        String message = apiResponse.getMessage();
+
+        assertEquals(false, apiResponse.isSuccess());
+        assertEquals("Assignment already exists with given Name : DA Assignment ", message);
+    }
+
+    @Test
+    @Order(3)
+    public void testCreatedByUser() throws Exception {
+        final AssignmentDto assignmentDto = new AssignmentDto();
+        assignmentDto.setAssignmentName("DA Assignment4");
+        assignmentDto.setAssignmentDescription("DA Practice");
+        assignmentDto.setComments("New assignment created");
+        assignmentDto.setPathAttachment1("FilePath1");
+        assignmentDto.setPathAttachment2("FilePath2");
+        assignmentDto.setPathAttachment3("FilePath3");
+        assignmentDto.setPathAttachment4("FilePath4");
+        assignmentDto.setPathAttachment5("FilePath5");
+        assignmentDto.setCreatedBy("U04");
+        assignmentDto.setBatchId(1);
+        assignmentDto.setGraderId("U01");
+        Calendar calendar = (Calendar) Calendar.getInstance();
+        calendar.set(Calendar.MONTH, 10);
+        calendar.set(Calendar.YEAR, 2023);
+        calendar.set(Calendar.DAY_OF_WEEK, 14);
+        Date dt = calendar.getTime();
+        assignmentDto.setDueDate(dt);
+
+        String jsonRequest = obj.writeValueAsString(assignmentDto);
+        MvcResult mvcResult = mockMvc.perform(post("/lms/assignments").contextPath("/lms")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType("application/json")
+                        .content(jsonRequest))
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        assertEquals(404, mvcResult.getResponse().getStatus());
+
+        ApiResponse apiResponse = obj.readValue(jsonResponse, ApiResponse.class);
+        String message = apiResponse.getMessage();
+
+        assertEquals(false, apiResponse.isSuccess());
+        assertEquals("User not found with Role(Admin/Staff) : U04 ", message);
+    }
+
+
+    @Test
+    @Order(4)
+    public void testValidRoleId() throws Exception {
+        final AssignmentDto assignmentDto = new AssignmentDto();
+        assignmentDto.setAssignmentName("DA Assignment5");
+        assignmentDto.setAssignmentDescription("DA Practice");
+        assignmentDto.setComments("New assignment created");
+        assignmentDto.setPathAttachment1("FilePath1");
+        assignmentDto.setPathAttachment2("FilePath2");
+        assignmentDto.setPathAttachment3("FilePath3");
+        assignmentDto.setPathAttachment4("FilePath4");
+        assignmentDto.setPathAttachment5("FilePath5");
+        assignmentDto.setCreatedBy("U02");
+        assignmentDto.setBatchId(1);
+        assignmentDto.setGraderId("U20");
+        Calendar calendar = (Calendar) Calendar.getInstance();
+        calendar.set(Calendar.MONTH, 10);
+        calendar.set(Calendar.YEAR, 2023);
+        calendar.set(Calendar.DAY_OF_WEEK, 14);
+        Date dt = calendar.getTime();
+        assignmentDto.setDueDate(dt);
+
+        String jsonRequest = obj.writeValueAsString(assignmentDto);
+        MvcResult mvcResult = mockMvc.perform(post("/lms/assignments").contextPath("/lms")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType("application/json")
+                        .content(jsonRequest))
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        assertEquals(404, mvcResult.getResponse().getStatus());
+
+        ApiResponse apiResponse = obj.readValue(jsonResponse, ApiResponse.class);
+        String message = apiResponse.getMessage();
+
+        assertEquals(false, apiResponse.isSuccess());
+        assertEquals("User not found with ID : U20 ", message);
+    }
+
+    @Test
+    @Order(5)
     public void testGetAllAssignments() throws Exception {
         final MvcResult mvcResult = mockMvc.perform(get("/lms/assignments").contextPath("/lms")
                         .header("Authorization", "Bearer " + token)
@@ -127,8 +243,8 @@ public class AssignmentControllerIT {
     }
 
     @Test
-    @Order(3)
-    public void getAssignmentById() throws Exception {
+    @Order(6)
+    public void testGetAssignmentById() throws Exception {
         final MvcResult mvcResult = mockMvc.perform(get("/lms/assignments/" + assignmentId)
                         .contextPath("/lms")
                         .header("Authorization", "Bearer " + token)
@@ -153,7 +269,26 @@ public class AssignmentControllerIT {
     }
 
     @Test
-    @Order(4)
+    @Order(7)
+    public void testGetAssignmentByInvalidId() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(get("/lms/assignments/" + 10)
+                        .contextPath("/lms")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType("application/json"))
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        assertEquals(404, mvcResult.getResponse().getStatus());
+
+        ApiResponse apiResponse = obj.readValue(jsonResponse, ApiResponse.class);
+        String message = apiResponse.getMessage();
+
+        assertEquals(false, apiResponse.isSuccess());
+        assertEquals("Assignment not found with Id : 10 ", message);
+    }
+
+    @Test
+    @Order(8)
     public void testUpdateAssignment() throws Exception {
         final AssignmentDto assignmentDto = new AssignmentDto();
         assignmentDto.setAssignmentName("DA Assignment2");
@@ -190,7 +325,87 @@ public class AssignmentControllerIT {
     }
 
     @Test
-    @Order(5)
+    @Order(9)
+    public void testAssignmentIdExist() throws Exception {
+        final AssignmentDto assignmentDto = new AssignmentDto();
+        assignmentDto.setAssignmentName("DA Assignment2");
+        assignmentDto.setAssignmentDescription("DA Practice");
+        assignmentDto.setComments("New assignment created");
+        assignmentDto.setPathAttachment1("FilePath1");
+        assignmentDto.setPathAttachment2("FilePath2");
+        assignmentDto.setPathAttachment3("FilePath3");
+        assignmentDto.setPathAttachment4("FilePath4");
+        assignmentDto.setPathAttachment5("FilePath5");
+        assignmentDto.setCreatedBy("U01");
+        assignmentDto.setBatchId(1);
+        assignmentDto.setGraderId("U01");
+        Calendar calendar = (Calendar) Calendar.getInstance();
+        calendar.set(Calendar.MONTH, 10);
+        calendar.set(Calendar.YEAR, 2023);
+        calendar.set(Calendar.DAY_OF_WEEK, 14);
+        Date dt = calendar.getTime();
+        assignmentDto.setDueDate(dt);
+
+        String jsonRequest = obj.writeValueAsString(assignmentDto);
+        MvcResult mvcResult = mockMvc.perform(put("/lms/assignments/" + 6)
+                        .contextPath("/lms")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType("application/json")
+                        .content(jsonRequest))
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        assertEquals(404, mvcResult.getResponse().getStatus());
+
+        ApiResponse apiResponse = obj.readValue(jsonResponse, ApiResponse.class);
+        String message = apiResponse.getMessage();
+
+        assertEquals(false, apiResponse.isSuccess());
+        assertEquals("Assignment not found with Id : 6 ", message);
+    }
+
+    @Test
+    @Order(10)
+    public void testUpdatedByUser() throws Exception {
+        final AssignmentDto assignmentDto = new AssignmentDto();
+        assignmentDto.setAssignmentName("DA Assignment2");
+        assignmentDto.setAssignmentDescription("DA Practice");
+        assignmentDto.setComments("New assignment created");
+        assignmentDto.setPathAttachment1("FilePath1");
+        assignmentDto.setPathAttachment2("FilePath2");
+        assignmentDto.setPathAttachment3("FilePath3");
+        assignmentDto.setPathAttachment4("FilePath4");
+        assignmentDto.setPathAttachment5("FilePath5");
+        assignmentDto.setCreatedBy("U04");
+        assignmentDto.setBatchId(1);
+        assignmentDto.setGraderId("U01");
+        Calendar calendar = (Calendar) Calendar.getInstance();
+        calendar.set(Calendar.MONTH, 10);
+        calendar.set(Calendar.YEAR, 2023);
+        calendar.set(Calendar.DAY_OF_WEEK, 14);
+        Date dt = calendar.getTime();
+        assignmentDto.setDueDate(dt);
+
+        String jsonRequest = obj.writeValueAsString(assignmentDto);
+        MvcResult mvcResult = mockMvc.perform(put("/lms/assignments/" + assignmentId)
+                        .contextPath("/lms")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType("application/json")
+                        .content(jsonRequest))
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        assertEquals(404, mvcResult.getResponse().getStatus());
+
+        ApiResponse apiResponse = obj.readValue(jsonResponse, ApiResponse.class);
+        String message = apiResponse.getMessage();
+
+        assertEquals(false, apiResponse.isSuccess());
+        assertEquals("User not found with Role(Admin/Staff) : U04 ", message);
+    }
+
+    @Test
+    @Order(11)
     public void testGetAssignmentsForBatch() throws Exception {
         final MvcResult mvcResult = mockMvc.perform(get("/lms/assignments/batch/" + 1).contextPath("/lms")
                         .header("Authorization", "Bearer " + token)
@@ -223,7 +438,25 @@ public class AssignmentControllerIT {
     }
 
     @Test
-    @Order(6)
+    @Order(12)
+    public void testGetAssignmentByInvalidBatchId() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(get("/lms/assignments/batch/" + 5).contextPath("/lms")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType("application/json"))
+                .andReturn();
+
+        String jsonResponse  = mvcResult.getResponse().getContentAsString();
+        assertEquals(404, mvcResult.getResponse().getStatus());
+
+        ApiResponse apiResponse = obj.readValue(jsonResponse, ApiResponse.class);
+        String message = apiResponse.getMessage();
+
+        assertEquals(false, apiResponse.isSuccess());
+        assertEquals("Assignments not found with BatchId : 5 ", message);
+    }
+
+    @Test
+    @Order(Integer.MAX_VALUE)
     public void testDeleteAssignment() throws Exception {
         final MvcResult mvcResult = mockMvc.perform(delete("/lms/assignments/" + assignmentId).contextPath("/lms")
                         .header("Authorization", "Bearer " + token)
